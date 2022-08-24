5e4dca638376c656abfeabd1225cab2ff5ad9b64
Lizeth Castillo <lizcg-8@gmail.com>
2022-08-24 12:23:31 -0500

    hover iconos

--- js/portafolio.js
+++ js/portafolio.js
@@ -1,1 +1,8 @@
 document.getElementById("portafolio").style.color = "#2196F3";
+const active = document.querySelector(".botonp1");
+const active2 = document.querySelector(".botonp");
+
+active.addEventListener("click", () => {
+  active.style.background = "#2196F3";
+  document.getElementById("botonp1").style.color = "#ffff";
+});
